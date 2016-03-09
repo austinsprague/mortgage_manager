4a91e276374cc458c94624fb5340d5d084df18d4
austinsprague <sprague.ak@gmail.com>
2016-03-08 16:00:42 -0800

    Uploading docs

--- script.js
+++ script.js
@@ -1,16 +1,101 @@
-var filepicker;
+filepicker.setKey("A2DtRrZBwRSGFqlZnYeY9z");
 
-var blob = {
-  url: 'https://www.filestackapi.com/api/file/AMltoa8ATECOsFTMduLz',
-  filename: 'hello.txt',
-  mimetype: 'text/plain',
-  isWriteable: true,
-  size: 100
-};
+$(document).ready(function(){
+  $('.list-group-item').click(function(){
+    $('.filepicker').toggle();
+  });
 
-filepicker.exportFile(
-  blob,
-  function(Blob){
-    console.log(Blob.url);
-  }
-);
+$('.list-group-item').click(function(){
+  $('.filepicker').
+  filepicker.pickAndStore(
+    {
+      multiple: true
+    },
+    {},
+    function onSucess(Blobs){
+      console.log(JSON.stringify(Blobs));
+      console.log('storing');
+    },
+    function onError(FPError){
+      console.log(FPERROR.toString());
+    },
+    function onProgress(FPProgress){
+      console.log('progress');
+    }
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $('.filepicker').click(function(){
+//         filepicker.pick(
+//
+//           function(Blob){
+//             console.log("Store successful:", JSON.stringify(Blob));
+//           },
+//           function(Blob){
+//             console.log(JSON.stringify(Blob));
+//           },
+//           function(FPError){
+//             console.log(FPError.toString());
+//           })
+//         filepicker.store(
+//           blob,
+//           {filename: 'cat#1.png'},
+//           function(new_blob){
+//         console.log(JSON.stringify(new_blob));
+//           })
+//           }
+//         )
+//     });
+
+
+
+
+// var blob = {
+//   url: 'https://www.filestackapi.com/api/file/A2DtRrZBwRSGFqlZnYeY9z',
+//   filename: 'cat.png',
+// };
+//
+// // filepicker.pick()
+// console.log("Storing", blob.filename);
+// filepicker.store(
+//   blob,
+//   {filename: 'theCat1.png'},
+//   function(new_blob){
+//     console.log(JSON.stringify(new_blob));
+//   }
+// );
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function(){
+//   $('.list-group-item').click(function(){
+//     $('.preview').toggle();
+//   });
+//
+// });
