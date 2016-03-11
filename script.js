f12ac6b053f0f96079e92f257236dce57553ba0d
austinsprague <sprague.ak@gmail.com>
2016-03-10 16:30:38 -0800

    Working side nav and upload

--- script.js
+++ script.js
@@ -1,54 +1,43 @@
 filepicker.setKey("A2DtRrZBwRSGFqlZnYeY9z");
 
+
+
 var ref = new Firebase("https://doc-doc-goose.firebaseio.com");
 var refTaxReturns = new Firebase("https://doc-doc-goose.firebaseio.com/tax-returns");
 var refPayStubs = new Firebase("https://doc-doc-goose.firebaseio.com/pay-stubs");
+var refProofFunds = new Firebase("https://doc-doc-goose.firebaseio.com/proof-funds");
+var refPhotoId = new Firebase("https://doc-doc-goose.firebaseio.com/photo-id");
 
 
 
 
-$(document).ready(function() {
-     $('#nav-tax-returns').click(function() {
-       $('.toggle-tax-returns').toggle();
-     });
-     $('#nav-pay-stubs').click(function() {
-       $('.toggle-pay-stubs').toggle();
-     });
-});
+function ToggleContent(id){
+  var $navId = $('#nav-' + id);
+  var $toggleClass = $('.toggle-' + id);
+
+  $navId.click(function(){
+    $toggleClass.toggle();
+  });
+}
+
+ToggleContent('tax-returns');
+ToggleContent('pay-stubs');
+ToggleContent('proof-funds');
+ToggleContent('photo-id');
 
 
-// function Filepicker(firebaseRef){
-//   filepicker.pickAndStore(
-//     {multiple: true},
-//     {},
-//     function onSucess(Blob){
-//       var json = (JSON.stringify(Blob));
-//       var content = JSON.parse(json);
-//
-//       firebaseRef.push({
-//         filename: content[0].filename,
-//         url: content[0].url
-//       });
-//       console.log('success');
-//     },
-//     function onError(FPError){
-//       console.log(FPERROR.toString());
-//     },
-//     function onProgress(FPProgress){
-//       console.log('progress');
-//     }
-//   );
-// }
 
-// $('#filepicker').click(Filepicker(refTaxReturns));
-
-$(document).ready(function(){
-  $('#filepicker').click(function(){
+function Filepicker(firebaseRef, id){
+  var $filepickerId = $('#filepicker-' + id);
+  $filepickerId.click(function(){
     filepicker.pickAndStore(
       {multiple: true},
       {},
-      function onSucess(content){
-        refTaxReturns.push({
+      function onSucess(Blob){
+        var json = (JSON.stringify(Blob));
+        var content = JSON.parse(json);
+
+        firebaseRef.push({
           filename: content[0].filename,
           url: content[0].url
         });
@@ -61,20 +50,33 @@ $(document).ready(function(){
         console.log('progress');
       }
     );
-  })
-})
+  });
+}
 
-function Snapshot(firebaseRef, resultId, badgeId, liId) {
+Filepicker(refTaxReturns, 'tax-returns');
+Filepicker(refPayStubs,'pay-stubs');
+Filepicker(refProofFunds,'proof-funds');
+Filepicker(refPhotoId,'photo-id');
+
+
+// var $badgeTaxReturn =
+// $('#badge-tax-returns').text($('.li-tax-returns').length)
+
+function Snapshot(firebaseRef, id) {
   firebaseRef.on('child_added', function(snapshot){
     var data = snapshot.val();
-    var $resultId = $('#' + resultId);
-    var $badgeId = $('#' + badgeId);
-    var $liId = $('#'+ liId)
+    var $resultId = $('#result-' + id);
+    var $badgeId = $('#badge-' + id);
+    var $liId = $('#li-'+ id)
 
     $resultId.append($('<div><a href="' + data.url + '">' + data.filename + '</a></div>'));
-    $badgeId.text($liId.length - 1);
+    $badgeId.text($liId.length);
   });
 }
 
 
-Snapshot(refTaxReturns, 'result-tax-returns', 'badge-tax-returns', 'li-tax-returns');
+
+Snapshot(refTaxReturns, 'tax-returns');
+Snapshot(refPayStubs, 'pay-stubs');
+Snapshot(refProofFunds, 'proof-funds');
+Snapshot(refPhotoId, 'photo-id');
